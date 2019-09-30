ae4772fb952b1a859c81c12e2650f5af41aa1602
ReidCampbell <reidscottcampbell@gmail.com>
2019-09-30 13:14:00 +0100

    refactored reduxform to be reusable

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = (props) => {
-    return (
-        <div>
-            StreamEdit
-        </div>
-    );
+class StreamEdit extends Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    
+    onSubmit = (formValues) => {
+
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>
+                <h3>Edit Stream</h3>
+                <StreamForm 
+                    initialValues={this.props.stream}
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
+        );
+    }
+    
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
