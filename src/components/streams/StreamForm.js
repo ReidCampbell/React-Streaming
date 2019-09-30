37e6ea9c9645adefbb70133fd63a349843577c26
ReidCampbell <reidscottcampbell@gmail.com>
2019-09-30 16:29:46 +0100

    Added the ability to stream from OBS

--- src/components/streams/StreamForm.js
+++ src/components/streams/StreamForm.js
@@ -32,7 +32,7 @@ onSubmit = (formValues) => {
             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
-                <button className="ui button primary">Submit</button>
+                <button className="ui button primary right floated">Submit</button>
             </form>
         );
     }
